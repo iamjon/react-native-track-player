53b83c21149ece8019584e3bc8c95ff8b77816bc
Jonathan <jonathanm@dayzz.com>
2018-10-10 15:11:08 +0300

    FML

--- android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
+++ android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
@@ -1,26 +1,34 @@
 package com.guichaguri.trackplayer.module;
 
 
+import android.content.Context;
 import android.net.Uri;
+import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.ui.PlayerNotificationManager;
-import com.guichaguri.trackplayer.service.Utils;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 
@@ -53,23 +61,44 @@ public class MusicModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void addJonathan(ReadableMap tracks) {
+        Log.e("PLAYER", "addJonathan");
         ReactContext context = getReactApplicationContext();
         HashMap track = tracks.toHashMap();
         track.get("id").toString();
+        setTitle(track.get("title").toString());
         TrackSelector trackSelector = new DefaultTrackSelector();
-        SimpleExoPlayer exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+        LoadControl loadControl = new DefaultLoadControl();
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(context);
+        //SimpleExoPlayer exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+        Looper looper = context.getCurrentActivity().getApplicationContext().getMainLooper();
+        SimpleExoPlayer exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector );
+
+        //exoPlayer.getApplicationLooper();
+
         DefaultDataSourceFactory dataSourceFactory = new  DefaultDataSourceFactory(context, "TrackPlayer");
         Uri audioSourceUri = Uri.parse(track.get("url").toString());
         MediaSource audioSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(audioSourceUri);
         exoPlayer.prepare(audioSource);
-        playerNotificationManager.setPlayer(exoPlayer);
+
+        try {
+            playerNotificationManager.setPlayer(exoPlayer);
+        } catch (Exception e) {
+            /* This is a generic Exception handler which means it can handle
+             * all the exceptions. This will execute if the exception is not
+             * handled by previous catch blocks.
+             */
+            System.out.println("Exception occurred");
+        }
+
+
         exoPlayer.setPlayWhenReady(true);
+        Log.e("PLAYER", "addJonathan end");
 
     }
 
     @ReactMethod
-    public void setupPlayer() {
-        Log.e(Utils.LOG, "setupPlayer");
+    public void setupPlayer(ReadableMap data) {
+        Log.e("PLAYER", "setupPlayer");
             playerNotificationManager = new PlayerNotificationManager( getReactApplicationContext(),"11",11, new DescriptionAdapter());
             // omit skip previous and next actions
             playerNotificationManager.setUseNavigationActions(false);
@@ -82,7 +111,7 @@ public class MusicModule extends ReactContextBaseJavaModule {
         playerNotificationManager.setBadgeIconType(0);
         playerNotificationManager.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
         playerNotificationManager.setOngoing(true);
-        Log.e(Utils.LOG, "setupPlayer end");
+        Log.e("PLAYER", "setupPlayer end");
     }
 
 
